be5a8f651b098a05562406f9bda43d0d881a39d7
TheRealMadzor <simon_lind_91@hotmail.com>
2015-05-27 15:01:20 +0200

    download script

--- Files.py
+++ Files.py
@@ -12,25 +12,27 @@ conn = swiftclient.client.Connection(auth_version=2, **config)
 Listan=[]
 totalsize=0
 (response, obj_list) = conn.get_container('GenomeData')
+pop = raw_input("What population do you want to download?: ")
 for obj in obj_list: 
-    if "GIH" in obj['name']:
+    if pop in obj['name'] and "bas" not in obj['name'] and "bai" not in obj['name']:
         totalsize+=float(obj['bytes'])
         Listan.append(obj['name']) 
 
 
-print str(totalsize/1000/1000/1000) +" GBytes"
+print str(totalsize/1024/1024/1024) +" GBytes"
 print str(len(Listan)) +" files"
-
-for i in range(0, len(Listan)-1):
-    
-    
-    start_time = time.time()
-    response, object_body =conn.get_object('GenomeData',Listan[i])
-    print("Done downloading " +Listan[i] +" time: %s seconds " % (time.time() - start_time))
-    start_time = time.time()
-    f = open('/home/ubuntu/GenomeData/'+Listan[i], 'wb')
-    f.write(object_body)
-    f.close()
-    print("Done writing " + Listan[i] +" time: %s seconds " % (time.time() - start_time))
+dl=raw_input("Start download? (y/n)")
+if dl =="y":
+	for i in range(0, len(Listan)-1):
+		
+		
+		start_time = time.time()
+		response, object_body =conn.get_object('GenomeData',Listan[i])
+		print("Done downloading " +Listan[i] +" time: %s seconds " % (time.time() - start_time))
+		start_time = time.time()
+		f = open('/home/ubuntu/GenomeData/'+Listan[i], 'wb')
+		f.write(object_body)
+		f.close()
+		print("Done writing " + Listan[i] +" time: %s seconds " % (time.time() - start_time))
 
 print "done"
